7026193c084b1d03979063197e193b285bd5fa25
baeian <ddoory1103@gmail.com>
2022-09-13 11:31:07 +0900

    goorm026 edited

--- goorm026.c
+++ goorm026.c
@@ -5,8 +5,8 @@ int maxh(int *height, int n)
    int i;
    int maxn = *height;
    for (i = 0; i < n; i++)
-      if (height[i] > maxn)
-         maxn = height[i];
+      if (*(height + i) > maxn)
+         maxn = *(height + i);
    return maxn;
 }
 int main()
@@ -18,7 +18,7 @@ int main()
 
    height = (int *)malloc(n);
    for (int i = 0; i < n; i++)
-      scanf("%d", &height[i]);
+      scanf("%d", height + i);
 
    printf("%d\n", maxh(height, n));
 
