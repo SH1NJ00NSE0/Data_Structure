e0464ad01b625251d0bc4cfe9621528e374ce8dc
baeian <ddoory1103@gmail.com>
2022-10-13 19:35:54 +0900

    goorm020(ex1)

---
+++ goorm020(ex1).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char s[10];
+	int len, i, p = -1;
+	scanf("%s", s);
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == 'A')
+		{
+			p = i;
+			break;
+		}
+	}
+	if (p != -1)
+		printf("%d번째\n", p + 1);
+	else
+		printf("not found\n");
+	return 0;
+}
