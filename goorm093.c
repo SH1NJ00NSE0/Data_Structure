0c91d8653ecd956ecc1b91d1dfe34d8849c69df2
baeian <ddoory1103@gmail.com>
2022-12-20 13:50:26 +0900

    FEAT : 그래프 관련 함수 추가

--- goorm093.c
+++ goorm093.c
@@ -12,7 +12,7 @@ typedef struct
 	int front, rear;
 } QueueType;
 
-void queue_init(QueueType *q)
+void queueInit(QueueType *q)
 {
 	q->front = q->rear = 0;
 }
@@ -27,28 +27,91 @@ int isFull(QueueType *q)
 	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
 }
 
-//삽입함수
+// 삽입함수
 void enqueue(QueueType *q, element item)
 {
-	if(isFull(q))
+	if (isFull(q))
 	{
 		printf("큐가 포화상태입니다");
 		return;
 	}
-	q->rear = (q->rear+1)%MAX_QUEUE_SIZE;
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
 	q->queue[q->rear] = item;
 }
 
-//삭제함수
+// 삭제함수
 element dequeue(QueueType *q)
 {
-	if(isEmpty(q))
+	if (isEmpty(q))
 	{
 		printf("큐가 공백상태입니다.");
 		return;
 	}
-	q->front = (q->front+1)%MAX_QUEUE_SIZE;
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
 	return q->queue[q->front];
+}
+
+#define MAX_VERTICES 50
+typedef struct GraphType
+{
+	int n;
+	int adj_mat[MAX_VERTICES][MAX_VERTICES];
+} GraphType;
+int visited[MAX_VERTICES];
+
+// 그래프 초기화
+void graphInit(GraphType *g)
+{
+	int r, c;
+	g->n = 0;
+	for (r = 0; r < MAX_VERTICES; r++)
+		for (c = 0; c < MAX_VERTICES; c++)
+			g->adj_mat[r][c] = 0;
+}
+
+// 정점 삽입 연산
+void insertVertex(GraphType *g, int v)
+{
+	if ((g->n) + 1 > MAX_VERTICES)
+	{
+		printf("그래프 : 정점의 개수 초과");
+		return;
+	}
+	g->n++;
+}
+
+// 간선 삽입 연산
+void insertEdge(GraphType *g, int start, int end)
+{
+	if (start >= g->n || end >= g->n)
+	{
+		printf("그래프 : 정점 번호 오류");
+		return;
+	}
+	g->adj_mat[start][end] = 1;
+	g->adj_mat[end][start] = 1;
+}
+
+void bfsMat(GraphType *g, int v)
+{
+	int w;
+	QueueType q;
+
+	queueInit(&q);
+	visited[v] = TRUE;
+	printf("%d 방문 -> ", v);
+	enqueue(&q, v);
+	while (!isEmpty(&q))
+	{
+		v = dequeue(&q);
+		for (w = 0; w < g->n; w++)
+			if (g->adj_mat[v][w] && !visited[w])
+			{
+				visited[w] = TRUE;
+				printf("%d 방문 -> ", w);
+				enqueue(&q, w);
+			}
+	}
 }
 
 int main()
