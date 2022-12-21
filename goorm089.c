17d11d2c47f3338130e45ee572aebff92f1acb99
baeian <ddoory1103@gmail.com>
2022-12-21 19:41:49 +0900

    ADD : goorm089

--- goorm089.c
+++ goorm089.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #define MAX_VERTEX 30
 
-typedef struct GraphNode // 그래프노드 구조체 정의
+// 그래프노드 구조체 정의
+typedef struct GraphNode
 {
 	int vertex;
 	struct GraphNode *link;
 } GraphNode;
 
-typedef struct GraphType // 그래프타입 구조체 정의
+// 그래프타입 구조체 정의
+typedef struct GraphType
 {
 	int n;
 	GraphNode *adjList[MAX_VERTEX];
@@ -40,9 +44,9 @@ void insertEdge(GraphType *g, int u, int v)
 		return;
 	}
 	node = (GraphNode *)malloc(sizeof(GraphNode)); // 노드타입 동적 할당
-	node->vertex =											  // 정점 저장
-		 node->link =										  // 배열에 저장된 정점 리스트의 처음 주소를 저장
-		 g->adjList[u] =									  // 현재 추가된 노드의 주소를 리스트의 주소값으로 저장;
+	node->vertex = v;										  // 정점 저장
+	node->link = g->adjList[u];						  // 배열에 저장된 정점 리스트의 처음 주소를 저장
+	g->adjList[u] = node;								  // 현재 추가된 노드의 주소를 리스트의 주소값으로 저장;
 }
 
 void print_adjList(GraphType *g)
@@ -52,11 +56,11 @@ void print_adjList(GraphType *g)
 	for (i = 0; i < g->n; i++)
 	{
 		printf("\n정점 %C의 인접리스트 ", i + 65);
-		p = // i번째 정점의 시작주소 저장
-			 while (p != NULL)
+		p = g->adjList[i]; // i번째 정점의 시작주소 저장
+		while (p != NULL)
 		{
-			printf(" -> %c", /*정점 출력*/); // 정점 0~4를 A~D로 출력
-			p =										// 다음 링크로 이동
+			printf(" -> %c", p->vertex + 'A'); // 정점 0~4를 A~D로 출력
+			p = p->link;							  // 다음 링크로 이동
 		}
 	}
 }
@@ -70,17 +74,17 @@ int main()
 	createGraph(G);
 	for (i = 0; i < 4; i++)
 		insertVertex(G, i);
-	insertEdge(G, /**/);
+	insertEdge(G, 0, 1);
 	insertEdge(G, 1, 3);
 	insertEdge(G, 1, 2);
-	insertEdge(G, /**/);
+	insertEdge(G, 1, 0);
 	insertEdge(G, 2, 3);
 	insertEdge(G, 2, 1);
 	insertEdge(G, 3, 2);
 	insertEdge(G, 3, 1);
 
 	printf("<G의 인접 리스트>");
-	print_adjList(/**/);
+	print_adjList(G);
 
 	return 0;
 }
